b327d2cc2e5e237157c6e506ac9154fac98ecf13
thomas <victoria.thomas-pro@hotmail.fr>
2016-06-05 19:56:33 +0200

    cringe form

--- app/src/main/java/fr/stephenrichard/cringe/MainActivity.java
+++ app/src/main/java/fr/stephenrichard/cringe/MainActivity.java
@@ -2,6 +2,8 @@ package fr.stephenrichard.cringe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        super.onCreate();
+        Firebase.setAndroidContext(this);
+
+        Firebase myFirebaseRef = new Firebase("https://project-5907554604004073121.firebaseio.com/");
+
+        myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
+
     }
+
+
+
 }

8f53371b6ee996f37b7491394013b06317c41847
stephenrichard <stephen.richard44@gmail.com>
2016-06-08 11:18:09 +0200

    Compensate fail previous commit

--- app/src/main/java/fr/stephenrichard/cringe/model/Cringe.java
+++ app/src/main/java/fr/stephenrichard/cringe/model/Cringe.java
@@ -12,36 +12,39 @@ import java.util.Map;
 public class Cringe {
     public Boolean isPrivate;
     public String author;
+    public String created_at;
     public Integer level;
     public String desc;
     public String uid;
-    public Double longitude;
-    public Double latitude;
+    public Double lng;
+    public Double lat;
 
     public Cringe() {
 
     }
 
-    public Cringe(Boolean isPrivate, String author, Integer level, String desc, String uid, Double longitude, Double latitude) {
+    public Cringe(Boolean isPrivate, String created_at, String author, Integer level, String desc, String uid, Double lng, Double lat) {
         this.isPrivate = isPrivate;
+        this.created_at = created_at;
         this.author = author;
         this.level = level;
         this.desc = desc;
         this.uid = uid;
-        this.longitude = longitude;
-        this.latitude = latitude;
+        this.lng = lng;
+        this.lat = lat;
     }
 
     @Exclude
     public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
         result.put("isPrivate", isPrivate);
+        result.put("created_at", created_at);
         result.put("author", author);
         result.put("level", level);
         result.put("desc", desc);
         result.put("uid", uid);
-        result.put("latitude", latitude);
-        result.put("longitude", longitude);
+        result.put("lat", lat);
+        result.put("lng", lng);
 
         return result;
     }

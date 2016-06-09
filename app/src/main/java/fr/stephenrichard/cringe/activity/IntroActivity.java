afb89cff2ade33dbb50dade4b0f5fb1eceb00055
stephenrichard <stephen.richard44@gmail.com>
2016-06-09 03:38:52 +0200

    Change splash screen / Remove login screen step

--- app/src/main/java/fr/stephenrichard/cringe/activity/IntroActivity.java
+++ app/src/main/java/fr/stephenrichard/cringe/activity/IntroActivity.java
@@ -3,17 +3,37 @@ package fr.stephenrichard.cringe.activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 import fr.stephenrichard.cringe.MainActivity;
 import fr.stephenrichard.cringe.adapter.ViewPagerAdapter;
@@ -25,36 +45,75 @@ public class IntroActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private ViewPagerAdapter viewPagerAdapter;
     private LinearLayout dotsLayout;
-    private Button btnNext, btnSkip;
     private int[] layouts;
     private TextView[] dots;
     private PrefManager prefManager;
 
+    private static final String TAG = "";
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private LoginButton loginButton;
+    CallbackManager callbackManager;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
         // Checking first time launch
         // If it's not the first time we launch, we don't display the intro slider
         prefManager = new PrefManager(this);
         if(!prefManager.isFirstTimeLaunch()) {
-            launchLoginScreen();
+            launchMainScreen();
             finish();
         }
 
-        // Make notification bar transparent
-        /*
-        if(Build.VERSION.SDK_INT >= 21) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-        */
-
         setContentView(R.layout.activity_intro);
+
+        mAuth = FirebaseAuth.getInstance();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("email", "public_profile", "user_friends"));
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError" + error);
+            }
+        });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    // ### LAUNCH THE NEW ACTIVITY HERE #####
+                    launchMainScreen();
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         dotsLayout = (LinearLayout) findViewById(R.id.layout_dots);
-        btnNext = (Button) findViewById(R.id.btn_next);
-        btnSkip = (Button) findViewById(R.id.btn_skip);
 
         layouts = new int[] {
                 R.layout.welcome_activity_1,
@@ -70,27 +129,20 @@ public class IntroActivity extends AppCompatActivity {
         viewPager.setAdapter(viewPagerAdapter);
         viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
 
-        btnSkip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                launchLoginScreen();
-            }
-        });
+    }
 
-        btnNext.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
-            @Override
-            public void onClick(View v) {
-                int current = getItem(+1);
-                if(current < layouts.length) {
-
-                    viewPager.setCurrentItem(current);
-                } else {
-                    launchLoginScreen();
-                }
-            }
-        });
-
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     // Add small dots (depends on intro steps)
@@ -102,7 +154,7 @@ public class IntroActivity extends AppCompatActivity {
             dots[i] = new TextView(this);
             dots[i].setText(Html.fromHtml("&#8226;"));
             dots[i].setTextSize(35);
-            dots[i].setTextColor(ContextCompat.getColor(this, R.color.light_grey));
+            dots[i].setTextColor(ContextCompat.getColor(this, R.color.dots_empty));
             dotsLayout.addView(dots[i]);
         }
 
@@ -115,27 +167,12 @@ public class IntroActivity extends AppCompatActivity {
         return viewPager.getCurrentItem() + i;
     }
 
-    // Call login screen
-    private void launchLoginScreen() {
-        prefManager.setIsFirstTimeLaunch(false);
-        startActivity(new Intent(this, LoginActivity.class));
-        finish();
-    }
-
 
     ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
 
         @Override
         public void onPageSelected(int position) {
             addBottomDots(position);
-
-            if(position == layouts.length - 1) {
-                btnNext.setText(getString(R.string.start));
-                btnSkip.setVisibility(View.GONE);
-            } else {
-                btnNext.setText(getString(R.string.next));
-                btnSkip.setVisibility(View.VISIBLE);
-            }
         }
 
         @Override
@@ -157,5 +194,39 @@ public class IntroActivity extends AppCompatActivity {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
         }
+    }
+
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:" + accessToken);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        if(!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(IntroActivity.this, "Authentication failed",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            launchMainScreen();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    // Call Main activity screen
+    private void launchMainScreen() {
+        prefManager.setIsFirstTimeLaunch(false);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }

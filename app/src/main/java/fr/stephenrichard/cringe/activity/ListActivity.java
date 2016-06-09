ef0887e4915292da2be952cbaf9c5ba92c0f8c59
thomas <victoria.thomas-pro@hotmail.fr>
2016-06-09 12:54:33 +0200

    fragment

--- app/src/main/java/fr/stephenrichard/cringe/activity/ListActivity.java
+++ app/src/main/java/fr/stephenrichard/cringe/activity/ListActivity.java
@@ -1,15 +1,30 @@
 package fr.stephenrichard.cringe.activity;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import fr.stephenrichard.cringe.R;
 
-public class ListActivity extends AppCompatActivity {
+public class ListActivity extends Fragment {
 
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_list, container, false);
+
     }
+
+//    @Override
+//    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//    }
 }
--- app/src/main/java/fr/stephenrichard/cringe/activity/MapActivity.java
+++ app/src/main/java/fr/stephenrichard/cringe/activity/MapActivity.java
@@ -1,15 +1,34 @@
 package fr.stephenrichard.cringe.activity;
 
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import fr.stephenrichard.cringe.R;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends Fragment {
 
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_map, container, false);
+
     }
+
+//    @Override
+//    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+//    }
 }
